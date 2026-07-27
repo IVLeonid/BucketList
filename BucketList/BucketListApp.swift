dd4ecaa1c017b9a349251a1b7355a5ade30605dd
Lenya <leonidivanov826@gmail.com>
2026-07-27 12:42:25 +0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Леонід Іванов on 27.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
