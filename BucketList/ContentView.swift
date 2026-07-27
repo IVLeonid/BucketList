c25b32deef39fbc2a7eb4928683c093278417bc1
Lenya <leonidivanov826@gmail.com>
2026-07-27 12:47:10 +0300

    test new token

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        Text("123")
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
