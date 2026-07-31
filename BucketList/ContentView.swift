4b163d93553c702abf39ded1dff92c98c0cb3a50
Lenya <leonidivanov826@gmail.com>
2026-08-01 00:00:50 +0300

    .

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,18 +6,33 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 48.46, longitude: 35.02),
+            span: MKCoordinateSpan(latitudeDelta: 0.15, longitudeDelta: 0.15)
+        )
+    )
+    
+    @State private var locations = [Location] ()
+    
     var body: some View {
-        
-        Text("123")
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        MapReader { proxy in
+            Map(initialPosition: startPosition) {
+                ForEach(locations) { location in
+                    Marker(location.name, coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+                }
+            }
+                .mapStyle(MapStyle.hybrid)
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local) {
+                        let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
+                        print("Tapped at \(coordinate)")
+                    }
+                }
         }
-        .padding()
     }
 }
 
