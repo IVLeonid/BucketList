c82a732a550fc32f5e213eb77422d108aa1b9336
Lenya <leonidivanov826@gmail.com>
2026-08-02 14:09:22 +0300

    .

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -17,21 +17,33 @@ struct ContentView: View {
     )
     
     @State private var locations = [Location] ()
+    @State private var selectedPlace: Location?
     
     var body: some View {
         MapReader { proxy in
-            Map(initialPosition: startPosition) {
+            Map(initialPosition: startPosition, selection: $selectedPlace) {
                 ForEach(locations) { location in
-                    Marker(location.name, coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(.circle)
+                    }
+                    .tag(location)
                 }
             }
-                .mapStyle(MapStyle.hybrid)
-                .onTapGesture { position in
-                    if let coordinate = proxy.convert(position, from: .local) {
-                        let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
-                        print("Tapped at \(coordinate)")
-                    }
+            .mapStyle(MapStyle.hybrid)
+            .onTapGesture { position in
+                if let coordinate = proxy.convert(position, from: .local) {
+                    let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
+                    locations.append(newLocation)
                 }
+            }
+            .sheet(item: $selectedPlace) { place in
+                Text(place.name)
+            }
         }
     }
 }
