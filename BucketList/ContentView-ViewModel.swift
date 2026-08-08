f3e53b830ec27716a5283d635092bca6adaa21b5
Lenya <leonidivanov826@gmail.com>
2026-08-08 14:58:11 +0300

    added ViewModel for ContentView. Added logic for saving and loading data via the ViewModel.

---
+++ BucketList/ContentView-ViewModel.swift
@@ -0,0 +1,75 @@
+//
+//  ContentView-ViewModel.swift
+//  BucketList
+//
+//  Created by Леонід Іванов on 08.08.2026.
+//
+
+import Foundation
+import SwiftUI
+import MapKit
+import LocalAuthentication
+
+extension ContentView {
+    @Observable
+    class ViewModel {
+        private(set) var locations: [Location]
+        var selectedPlace: Location?
+        let savePath = URL.documentsDirectory.appending(path: "SavedPlaces")
+        var isUnlocked = false
+        
+        func addLocation(at point: CLLocationCoordinate2D) {
+            let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: point.latitude, longitude: point.longitude)
+            locations.append(newLocation)
+            save()
+        }
+        
+        func update(location: Location) {
+            guard let selectedPlace else { return }
+            
+            if let index = locations.firstIndex(of: selectedPlace) {
+                locations[index] = location
+            }
+            save()
+        }
+        
+        //downloading data with init()
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
+        
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+            } catch {
+                print("Unable to save data")
+            }
+        }
+        
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+            
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "We need to unlock your data."
+                
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                    
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        // error
+                    }
+                }
+            } else {
+                // no biometrick
+            }
+        }
+    }
+}
--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -16,38 +16,42 @@ struct ContentView: View {
         )
     )
     
-    @State private var locations = [Location] ()
-    @State private var selectedPlace: Location?
+    @State private var viewModel = ViewModel()
     
     var body: some View {
-        MapReader { proxy in
-            Map(initialPosition: startPosition, selection: $selectedPlace) {
-                ForEach(locations) { location in
-                    Annotation(location.name, coordinate: location.coordinate) {
-                        Image(systemName: "star.circle")
-                            .resizable()
-                            .foregroundStyle(.red)
-                            .frame(width: 44, height: 44)
-                            .background(.white)
-                            .clipShape(.circle)
-                    }
-                    .tag(location)
-                }
-            }
-            .mapStyle(MapStyle.hybrid)
-            .onTapGesture { position in
-                if let coordinate = proxy.convert(position, from: .local) {
-                    let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
-                    locations.append(newLocation)
-                }
-            }
-            .sheet(item: $selectedPlace) { place in
-                EditView(location: place) { newLocation in
-                    if let index = locations.firstIndex(of: place) {
-                        locations[index] = newLocation
+        if viewModel.isUnlocked {
+            MapReader { proxy in
+                Map(initialPosition: startPosition, selection: $viewModel.selectedPlace) {
+                    ForEach(viewModel.locations) { location in
+                        Annotation(location.name, coordinate: location.coordinate) {
+                            Image(systemName: "star.circle")
+                                .resizable()
+                                .foregroundStyle(.red)
+                                .frame(width: 44, height: 44)
+                                .background(.white)
+                                .clipShape(.circle)
+                        }
+                        .tag(location)
                     }
                 }
+                .mapStyle(MapStyle.hybrid)
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local) {
+                        viewModel.addLocation(at: coordinate)
+                    }
+                }
+                .sheet(item: $viewModel.selectedPlace) { place in
+                    EditView(location: place) {
+                        viewModel.update(location: $0)
+                    }
+                }
             }
+        } else {
+            Button("Unlock Pleaces", action: viewModel.authenticate)
+                .padding()
+                .background(.blue)
+                .foregroundStyle(.white)
+                .clipShape(.capsule)
         }
     }
 }
